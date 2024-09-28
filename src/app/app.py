1c5782e45a12e84416cf817456872833b621ed10
Dylan Noell <dmnoell@mail.roanoke.edu>
2024-09-28 00:35:22 -0400

    jinja testins

--- src/app/app.py
+++ src/app/app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/<name>')
+def home(name=None):
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
